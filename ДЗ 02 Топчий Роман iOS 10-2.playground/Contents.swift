2ae2496159ad47e3fc516e351c024c2394ec3155
RomanTopchii <roma.topchiy@me.com>
2019-09-09 17:28:24 +0300

    Add multiply function

--- ДЗ 02 Топчий Роман iOS 10-2.playground/Contents.swift
+++ ДЗ 02 Топчий Роман iOS 10-2.playground/Contents.swift
@@ -4,12 +4,18 @@ var str = "Hello, playground"
 
 let a = 12
 var b = 234
+
 func sum(first : Int, second : Int) -> Int {
     return first + second
 }
-
-let c = sum(first: a, second: b)
 func substraction(minuend : Int, subtrahend : Int) -> Int {
     return minuend - subtrahend
 }
- let d = substraction(minuend: a, subtrahend: b)
+func multiply(first : Int, second : Int) -> Int {
+    return first + second
+}
+
+
+let c = sum(first: a, second: b)
+let d = substraction(minuend: a, subtrahend: b)
+let m = multiply(first: a, second: b)
