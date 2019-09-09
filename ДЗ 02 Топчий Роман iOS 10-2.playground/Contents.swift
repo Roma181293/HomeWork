5d8167702c54a794aa6070d8cc463be1f92debd5
RomanTopchii <roma.topchiy@me.com>
2019-09-09 17:16:55 +0300

    добавил функцию разности

--- ДЗ 02 Топчий Роман iOS 10-2.playground/Contents.swift
+++ ДЗ 02 Топчий Роман iOS 10-2.playground/Contents.swift
@@ -4,3 +4,8 @@ var str = "Hello, playground"
 
 let a = 12
 var b = 234
+
+func substraction(minuend : Int, subtrahend : Int) -> Int {
+    return minuend - subtrahend
+}
+ let d = substraction(minuend: a, subtrahend: b)
