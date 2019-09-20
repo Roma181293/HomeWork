232e2bfcb72de763e85471a454adf74aa412dfac
Roman Topchii <roma.topchiy@me.com>
2019-09-21 01:27:23 +0300

    ДЗ 05 переделано кроме 5.6, 5.7

--- ДЗ 05 Топчий Роман iOS 19-2.playground/Contents.swift
+++ ДЗ 05 Топчий Роман iOS 19-2.playground/Contents.swift
@@ -8,8 +8,8 @@ import UIKit
 print("5.1. Найти сумму  элементов, находящихся между первым и последним отрицательными элементами.")
 var array = [1,-1,1,1,-1,1,-1,1]
 print(array)
-var leftIndexOfMin = -1
-var rightIndexOfMin = array.count
+var leftIndexOfMin : Int?
+var rightIndexOfMin : Int?
 for (index, value) in array.enumerated() {
     if value < 0 {
         leftIndexOfMin = index
@@ -27,32 +27,31 @@ for index in 0..<array.count{
 //print(leftIndexOfMin)
 //print(rightIndexOfMin)
 var sum = 0
-if rightIndexOfMin > leftIndexOfMin && leftIndexOfMin != -1 && rightIndexOfMin != array.count{
-    for index in leftIndexOfMin+1..<rightIndexOfMin {
+if rightIndexOfMin! > leftIndexOfMin! && leftIndexOfMin != nil && rightIndexOfMin != nil {
+    for index in leftIndexOfMin! + 1..<rightIndexOfMin! {
         sum += array[index]
     }
     print("сумма  элементов, находящихся между первым и последним отрицательными элементами = \(sum)\n")
 }
-else if leftIndexOfMin == -1 && rightIndexOfMin == array.count {
+else if leftIndexOfMin == nil && rightIndexOfMin == nil {
     print("недостаточное кол-во отрицательных чисел. нет либо правого, либо левого или и правого и левого отрицательного значения")
 }
 
 
 print("5.2. Преобразовать массив так, чтобы сначала шли все отрицательные элементы, а потом положительные(0 считать положительным). (не использовать встроенный remove, insert, append)")
-array = [2, 8, 4, -5, 2, -1, 0, -40]
+array = [2,8,4,-5,1,-6,0,]
 print(array)
-var array2 = [Int]()//Array(repeating: 0, count: array.count)
-for element in array {
-    if element < 0{
-        array2 += [element]
+var negativeCounter = 0
+for (index, value) in array.enumerated() {
+    if value < 0 {
+        let buf = value
+        for j in  0..<index - negativeCounter  {
+            array[index-j] = array[index-j-1]
+        }
+        array[negativeCounter] = buf
+        negativeCounter += 1
     }
 }
-for element in array {
-    if element >= 0{
-        array2 += [element]
-    }
-}
-array = array2
 print(array)
 
 
@@ -73,8 +72,8 @@ for element in array {
         counterOfPositivNumbers += 1
     }
 }
-
 print("Количество положительных элементов = \(counterOfPositivNumbers) \nКоличество отрицательных элементов = \(counterOfNegativeNumbers) \nСумма положительных элементов = \(sumOfPositivNumbers) \nСумма отрицательных элементов = \(sumOfNegativeNumbers) \n")
+
 
 print("5.4. Сжать массив, удалив из него все 0, и заполнить освободившиеся справа элементы значениями -1. (не использовать встроенный remove, insert, append)")
 array = [4,0,2,0,3,4,0,3,4,5,0,0,5]
@@ -100,8 +99,9 @@ print(array)
 
 print("\n5.5.Написать программу, определяющую сумму элементов массива, находящихся в массиве после первого элемента со значением 0.")
 array = [1,1,0,0,1,1,1]
+
 print(array)
-var indexOfFirstZero : Int = -1
+var indexOfFirstZero : Int?
 sum = 0
 for (index,value) in array.enumerated() {
     if value == 0 {
@@ -110,16 +110,16 @@ for (index,value) in array.enumerated() {
         break
     }
 }
-for index in indexOfFirstZero + 1..<array.count {
-    sum += array[index]
-}
-
-if indexOfFirstZero == -1 {
-    print("В массиве отсутствует нулевое значение\n")
-}
-else {
+if indexOfFirstZero != nil  {
+    for index in indexOfFirstZero! + 1..<array.count {
+        sum += array[index]
+    }
     print("суммa элементов массива, находящихся в массиве после первого элемента со значением 0 = \(sum)\n")
 }
+else {
+    print("В массиве отсутствует нулевое значение\n")
+}
+
 
 print("5.6. Дан массив случайных чисел в диапазоне от -20 до +20. Необходимо найти позиции крайних отрицательных элементов (самого левого отрицательного элемента и самого правого отрицательного элемента) и отсортировать элементы, находящиеся между ними. ")
 array = []
@@ -183,3 +183,68 @@ arrayB = arrayB.sorted(by: <)
 array = arrayA + [randomNumber] + arrayB
 
 print(array)
+
+
+
+
+
+
+
+
+
+// print("5.6. Дан массив случайных чисел в диапазоне от -20 до +20. Необходимо найти позиции крайних отрицательных элементов (самого левого отрицательного элемента и самого правого отрицательного элемента) и отсортировать элементы, находящиеся между ними. ")
+// var array = [20,-1,18,-3,16,-5,14,-7,12,-9,10,-11,8,-13,6,-14,4,-15,2,-17]
+// print(array)
+
+// var bufferArray = [Int]()
+// var leftIndexOfMin : Int?                   //delete var
+// var rightIndexOfMin : Int?                  //delete var
+// for (index, value) in array.enumerated() {
+//     if value < 0 {
+//         leftIndexOfMin = index
+//         break
+//     }
+// }
+// for index in 0..<array.count{
+//     if array[array.count - 1 - index] < 0 {
+//         rightIndexOfMin = array.count - 1 - index
+//         break
+//     }
+// }
+
+// if leftIndexOfMin != nil && rightIndexOfMin != nil{
+   
+  
+//     for i in leftIndexOfMin! + 1..<rightIndexOfMin! {
+//         var localMinIndex : Int?
+//         var localMin = array[i]
+//         print("start \(localMinIndex) - \(localMin)")
+//         //поиск минимума
+//         for j in leftIndexOfMin! + 1..<rightIndexOfMin!-1 {
+//            if array[j] < localMin {
+//                 localMin = array[j]
+//            }
+//         }
+//         print("find \(localMinIndex) - \(localMin)")
+//         //поиск индекса минимума
+//         for l in leftIndexOfMin! + 1..<rightIndexOfMin!-1 {
+//            if array[l] == localMin {
+//                 localMinIndex = l
+//            }
+//         }
+//          print("find \(localMinIndex!) - \(localMin)")
+//         print("find locIndex and i \(localMinIndex!) - \(i)")
+//         for k in 0..<localMinIndex!-i+1 {
+//             array[localMinIndex!-k] = array[localMinIndex!-k-1]
+//         }
+    
+//         array[i] = localMin
+//         localMin = 1234567
+//         print(array)
+//     }
+//     print(array)
+   
+// }
+// else {
+//     print("Не найдено два отрицательных числа в массиве!")
+// }
