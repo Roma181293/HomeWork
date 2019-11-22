b9ff5372c7d186a913a279423f0598ef4af8af97
RomanTopchii <roma.topchiy@me.com>
2019-11-22 09:03:54 +0200

    ДЗ 15 выполнено

---
+++ ДЗ 15 Топчий Роман iOS 19-2/ДЗ 15 Топчий Роман iOS 19-2/main.swift
@@ -0,0 +1,298 @@
+//
+//  main.swift
+//  ДЗ 15 Топчий Роман iOS 19-2
+//
+//  Created by Roman Topchii on 19.11.2019.
+//  Copyright © 2019 Roman Topchii. All rights reserved.
+//
+
+import Foundation
+
+print("""
+Создать класс Family (семья), который содержит ссылки на маму, папу, массив детей.
+
+В семье создать маму, папу, 2 детей (сильными ссылками). У каждого человека должно быть имя.
+
+У класса мама:
+1) создать метод childrenGoToSleep(), который вызывает метод goToSleep() у каждого ребенка;
+2) создать метод husbandBuyBoots(), который вызывает метод buyBoots() у папы;
+
+У класса папа:
+1) создать метод childrenGoToSleep(), который вызывает метод goToSleep() у каждого ребенка;
+2) создать метод wifeCookMeat(), который вызывает метод cookMeat() у мамы;
+
+У класса ребенок:
+1) создать метод letsPlayFootbal(), который вызывает метод playFootbal() у каждого ребенка;
+2) создать метод momCookMeat(), который вызывает метод cookMeat() у мамы;
+
+Создать семью  (экземпляр класса Family)
+Вызвать все эти методы для демонстрации.
+
+Для каждого класса создать счетчик живых объектов и выводить на экран количество живых экземпляров в init и deinit.
+
+После демонстрации удалить экземпляр семья.
+После удаления семьи не должно остаться ни одного живого объекта.
+""")
+
+
+class Mother {
+    var name : String
+    weak var husband : Father?
+    var children : [Child] = []
+    
+    static var objectCounter : Int = 0
+    
+    init(name : String) {
+        self.name = name
+        Mother.objectCounter += 1
+    }
+    
+    func childrenGoToSleep() {
+        print("-Mom: Children, go to sleep!!!")
+        for child in children {
+            child.goToSleep()
+        }
+    }
+    func husbandBuyBoots(){
+        print("-Mom: Husband, buy me this boots!!!")
+        husband?.buyBoots()
+    }
+    func cookMeat(){
+        print("-Mom: I'm cooking the meat")
+    }
+    
+    lazy var childrenGoToSleep_closure: ()->() =  {[children] in
+        for child in children {
+            child.goToSleep()
+        }
+    }
+    lazy var husbandBuyBoots_closure: ()->() =  {[weak husband] in
+        guard let husband = husband else {return}
+        husband.buyBoots()
+    }
+    
+    
+    deinit {
+        print("\(name) - deinited")
+        Mother.objectCounter -= 1
+    }
+}
+
+class Father {
+    var name : String
+    weak var wife : Mother?
+    var children : [Child] = []
+    
+    static var objectCounter : Int = 0
+    
+    init(name : String) {
+        self.name = name
+        Father.objectCounter += 1
+    }
+    func childrenGoToSleep() {
+        print("-Father: Children, go to sleep!!!")
+        for child in children {
+            child.goToSleep()
+        }
+    }
+    func wifeCookMeat() {
+        print("-Father: Wife, lets start cook the meat!!!")
+        wife?.cookMeat()
+    }
+    func buyBoots() {
+        print("-Father: I'm buying the boots")
+    }
+    
+    
+    lazy var childrenGoToSleep_closure: ()->() =  {[children] in
+        for child in children {
+            child.goToSleep()
+        }
+    }
+    lazy var wifeCookMeat_closure: ()->() =  {[weak wife] in
+        guard let wife = wife else {return}
+        wife.cookMeat()
+    }
+    
+    deinit {
+        print("\(name) - deinited")
+        Father.objectCounter -= 1
+    }
+}
+
+class Child {
+    var name : String
+    
+    weak var mother : Mother?
+    weak var father : Father?
+    
+    static var objectCounter : Int = 0
+    
+    init(name : String) {
+        self.name = name
+        Child.objectCounter += 1
+    }
+    func letsPlayFootbal(){
+        print("-\(name): Lets play football")
+        if let mom = mother{
+            for index in 0..<mom.children.count {
+                if !(mom.children[index] === self) {
+                    mom.children[index].playFootbal(name: name)
+                }
+            }
+        }
+    }
+    func goToSleep(){
+        print("-\(name): Im going to sleep")
+    }
+    func playFootbal(name : String){
+        print("-\(self.name): Ok, \(name), lets play")
+    }
+    func momCookMeat(){
+        print("-\(name): Mom, please cook a meat")
+        mother?.cookMeat()
+    }
+    lazy var letsPlayFootbal_closure: ()->() =  {[weak self] in
+        guard let `self` = self else {return}
+        guard let mother = self.mother else {return}
+        print("-\(self.name): Lets play football")
+        for index in 0..<mother.children.count {
+            if !(mother.children[index] === self) {
+                mother.children[index].playFootbal(name: self.name)
+            }
+        }
+    }
+    lazy var momCookMeat_closure: ()->() =  {[weak self] in
+        guard let `self` = self else {return}
+        print("-\(self.name): Mom, please cook a meat")
+        guard let mother = self.mother else {return}
+        mother.cookMeat()
+        
+        
+    }
+    deinit {
+        print("\(name) - deinited")
+        Child.objectCounter -= 1
+    }
+}
+
+
+
+class Family {
+    var mother : Mother?
+    var father : Father?
+    var children : [Child] = []
+    
+    static var objectCounter : Int = 0
+    init(){
+        Family.objectCounter += 1
+    }
+    deinit {
+        print("Family - deinited")
+        Family.objectCounter -= 1
+    }
+}
+
+
+
+var family : Family! = Family()
+
+print("\nSTART\nMother.objectCounter:\(Mother.objectCounter)")
+print("Father.objectCounter:\(Father.objectCounter)")
+print("Child.objectCounter:\(Child.objectCounter)")
+
+family.mother = Mother(name: "Anna")
+family.father = Father(name: "Alex")
+family.children = [Child(name: "Roma"), Child(name : "Kate")]
+
+print("\nMIDDLE\nMother.objectCounter:\(Mother.objectCounter)")
+print("Father.objectCounter:\(Father.objectCounter)")
+print("Child.objectCounter:\(Child.objectCounter)")
+
+family.mother?.husband = family.father
+family.father?.wife = family.mother
+
+family.mother?.children = family.children
+family.father?.children = family.children
+
+
+for i in 0..<family.children.count {
+    family.children[i].father = family.father
+    family.children[i].mother = family.mother
+}
+
+family.mother?.childrenGoToSleep()
+family.father?.childrenGoToSleep()
+
+family.father?.wifeCookMeat()
+family.mother?.husbandBuyBoots()
+
+family.children[0].momCookMeat()
+family.children[1].momCookMeat()
+
+for i in 0..<family.children.count {
+    family.children[i].letsPlayFootbal()
+}
+family = nil
+
+
+print("\nFINISH\nMother.objectCounter:\(Mother.objectCounter)")
+print("Father.objectCounter:\(Father.objectCounter)")
+print("Child.objectCounter:\(Child.objectCounter)")
+print("Family.objectCounter:\(Child.objectCounter)")
+
+
+
+
+
+
+
+
+
+
+print("\n=================================================\nCAPTURE REFERENCE IN CLOSURE\n=================================================")
+family = Family()
+
+print("\nSTART\nMother.objectCounter:\(Mother.objectCounter)")
+print("Father.objectCounter:\(Father.objectCounter)")
+print("Child.objectCounter:\(Child.objectCounter)")
+
+family.mother = Mother(name: "Anna")
+family.father = Father(name: "Alex")
+family.children = [Child(name: "Roma"), Child(name : "Kate")]
+
+print("\nMIDDLE\nMother.objectCounter:\(Mother.objectCounter)")
+print("Father.objectCounter:\(Father.objectCounter)")
+print("Child.objectCounter:\(Child.objectCounter)")
+
+family.mother?.husband = family.father
+family.father?.wife = family.mother
+
+family.mother?.children = family.children
+family.father?.children = family.children
+
+
+for i in 0..<family.children.count {
+    family.children[i].father = family.father
+    family.children[i].mother = family.mother
+}
+
+family.mother?.childrenGoToSleep_closure()
+family.father?.childrenGoToSleep_closure()
+
+family.father?.wifeCookMeat_closure()
+family.mother?.husbandBuyBoots_closure()
+
+family.children[0].momCookMeat_closure()
+family.children[1].momCookMeat_closure()
+
+for i in 0..<family.children.count {
+    family.children[i].letsPlayFootbal_closure()
+}
+family = nil
+
+
+print("\nFINISH\nMother.objectCounter:\(Mother.objectCounter)")
+print("Father.objectCounter:\(Father.objectCounter)")
+print("Child.objectCounter:\(Child.objectCounter)")
+print("Family.objectCounter:\(Child.objectCounter)")
