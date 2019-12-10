ccbc473c2b1b6e8ba82cc2bd731b8a5597ce1ae4
RomanTopchii <roma.topchiy@me.com>
2019-12-10 22:39:13 +0200

    ДЗ 19

---
+++ ДЗ 19 Топчий Роман iOS 19-2/ДЗ 19 Топчий Роман iOS 19-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ДЗ 19 Топчий Роман iOS 19-2
+//
+//  Created by Roman Topchii on 10.12.2019.
+//  Copyright © 2019 Roman Topchii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
