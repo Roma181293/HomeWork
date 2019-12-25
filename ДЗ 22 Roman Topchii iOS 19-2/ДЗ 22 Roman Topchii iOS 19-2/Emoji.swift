83a5c2a02c18fe8bde2106ea3da3415b7bc08703
RomanTopchii <roma.topchiy@me.com>
2019-12-25 19:31:36 +0200

    HW 22 done.

---
+++ ДЗ 22 Roman Topchii iOS 19-2/ДЗ 22 Roman Topchii iOS 19-2/Emoji.swift
@@ -0,0 +1,16 @@
+//
+//  Emoji.swift
+//  ДЗ 22 Roman Topchii iOS 19-2
+//
+//  Created by Roman Topchii on 25.12.2019.
+//  Copyright © 2019 Roman Topchii. All rights reserved.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
